80559966072738531e603a9191281c5dfbc7ab27
BuLbl4 <113981565+BuLbl4@users.noreply.github.com>
2025-03-20 00:34:07 +0300

    update

--- WeatherTask/WeatherDataService.Persistence/EfContext/EfFactory.cs
+++ WeatherTask/WeatherDataService.Persistence/EfContext/EfFactory.cs
@@ -6,13 +6,19 @@ namespace WeatherDataService.Persistence.EfContext;
 
 public class EfFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
 {
+    private readonly IConfiguration _configuration;
+
+    public EfFactory(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
     public ApplicationDbContext CreateDbContext(string[] args)
     {
+        var connectionString = _configuration.GetConnectionString("DefaultConnection");
         var config = new ConfigurationBuilder().Build();
-        
+
         var optionBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
-        
-        optionBuilder.UseNpgsql("Host=localhost;Port=5432;Username=postgres;Password=Bulat2004;Database=Weather");
+        optionBuilder.UseNpgsql(connectionString);
         
         return new ApplicationDbContext(optionBuilder.Options);
     }
